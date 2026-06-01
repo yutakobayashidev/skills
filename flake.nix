d0e789cfb08df93205388ea44ccc8196019fb104
yutakobayashidev <hi@yutakobayashi.com>
2026-06-02 03:53:35 +0900

    feat: add flake.nix with waza package

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Agent skills collection for AI coding agents";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages = {
+            waza = pkgs.callPackage ./pkgs/waza { };
+          };
+        };
+    };
+}
